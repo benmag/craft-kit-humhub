5e91d3227605cd07e7e61b69e1f139fd33619c2d
Ben M <benmag@users.noreply.github.com>
2017-04-28 16:49:22 +1000

    Update command - pass params in

--- mason-json/mysql.js
+++ mason-json/mysql.js
@@ -6,7 +6,7 @@ module.exports = {
         './storage/data/mysql':'/var/lib/mysql'
     },
     ports: ["3306:3306"],
-    command: 'mysqld --sql-mode="STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"',
+    command: "--sql-mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION",
     environment: {
         'MYSQL_DATABASE': 'demo',
         'MYSQL_USER': 'demo',
